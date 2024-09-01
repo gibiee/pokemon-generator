a94df5c77643e23d1ce13e42033efeb5eeb349d9
gibiee <gibiee@naver.com>
2024-09-01 18:02:35 +0900

    demo prototype

--- app.py
+++ app.py
@@ -7,32 +7,71 @@ import torch
 import sys
 sys.path.append('./stylegan-xl')
 import dnnlib, legacy
+from torch_utils import gen_utils
 
 NETWORK = "https://s3.eu-central-1.amazonaws.com/avg-projects/stylegan_xl/models/pokemon1024.pkl"
-SEED = 42
-
-np.random.seed(SEED)
-torch.manual_seed(SEED)
-
+CLASS_NAMES = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
+CLASS_FEATURES = {cname: np.load(f'class_features/{cname}.npy') for cname in CLASS_NAMES}
 device = torch.device('cuda')
+
 with dnnlib.util.open_url(NETWORK) as f:
     G = legacy.load_network_pkl(f)['G_ema']
     G = G.eval().requires_grad_(False).to(device)
 
+def reset_values(*args) :
+    return [0 for _ in range(len(args))]
+            
 def randomize_values(*args) :
     return [round(random.random(), 2) for _ in range(len(args))]
 
-def generate_pokemon(normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy) :
+def generate_base_pokemon() :
+    random_seed = random.randint(0, 2**32 - 1)
+    base_w = gen_utils.get_w_from_seed(G, batch_sz=1, device=device, seed=random_seed)
+    output = gen_utils.w_to_img(G, base_w, to_np=True)[0]
+    output_img = Image.fromarray(output, 'RGB')
+    return (base_w, output_img), output_img, [output_img]
+
+def apply_class_feature(base_state, base_ratio, normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy) :
+    if base_state is None :
+        raise gr.Error('Base Pokémon is required.')
+    
     class_values = [normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy]
-    if 0 in class_values :
-        raise gr.Error('0 values are not allowed')
+    if sum(class_values) == 0 :
+        # output = gen_utils.w_to_img(G, base_state, to_np=True)[0]
+        # return Image.fromarray(output, 'RGB')
+        return base_state[-1]
     
+    class_values = np.array(class_values)
+    target_feature = None
+    for i, class_value in enumerate(class_values) :
+        class_name = CLASS_NAMES[i]
+        class_feature = CLASS_FEATURES[class_name]
+
+        if target_feature is None :
+            target_feature = class_feature * class_value
+        else :
+            target_feature += class_feature * class_value
     
-    return Image.new('RGB', (256,256), (255,128,128))
+    target_feature = torch.from_numpy(target_feature).to(device)
+    target_feature = target_feature.repeat(1, G.num_ws, 1)
+
+    base_w = base_state[0]
+    edit_w = (base_w * base_ratio) + (target_feature * (1 - base_ratio))
+    output_edit = gen_utils.w_to_img(G, edit_w, to_np=True)[0]
+    return Image.fromarray(output_edit, 'RGB')
+
+def add_image_to_gallery(gallery, img) :
+    gallery.append(img)
+    return gallery
 
 with gr.Blocks() as demo :
+    gr.Markdown('# Pokémon Generator')
+    gr.Markdown('Generate the base Pokémon. Then, apply class features to edit the base Pokémon.')
     with gr.Row() :
         with gr.Column() :
+            btn_generate = gr.Button('Generate a Pokémon', variant='primary')
+            base_state = gr.State()
+
             with gr.Row() :
                 normal = gr.Slider(label='Normal', minimum=0, maximum=1, value=0, step=0.01)
                 fire = gr.Slider(label='Fire', minimum=0, maximum=1, value=0, step=0.01)
@@ -54,22 +93,46 @@ with gr.Blocks() as demo :
                 fairy = gr.Slider(label='Fairy', minimum=0, maximum=1, value=0, step=0.01)
 
             with gr.Row() :
-                btn_random = gr.Button('Random', variant='secondary')
-                btn_generate = gr.Button('Generate', variant='primary')
+                btn_zero = gr.Button('Reset', variant='secondary')
+                btn_random = gr.Button('Randomize', variant='secondary')
+                btn_edit = gr.Button('Apply class feature', variant='primary')
+            
+            base_ratio = gr.Slider(label='Base ratio', minimum=0, maximum=1, value=0.5, step=0.01,
+                                   info="The parameter that determines how much the base Pokémon's form is retained.")
         
-        output_img = gr.Image(label='Output', image_mode='RGB', type='pil')
+        with gr.Column() :
+            output_img = gr.Image(label='Output', image_mode='RGB', type='pil', interactive=False)
+            output_gallery = gr.Gallery(label='Gallery', columns=5, interactive=False)
+
+    btn_zero.click(
+        fn=reset_values,
+        inputs=[normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy],
+        outputs=[normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy]
+    )
 
     btn_random.click(
         fn=randomize_values,
         inputs=[normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy],
         outputs=[normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy]
     )
+
     btn_generate.click(
-        fn=generate_pokemon,
-        inputs=[normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy],
+        fn=generate_base_pokemon,
+        inputs=None,
+        outputs=[base_state, output_img, output_gallery],
+        concurrency_id='gpu')
+    
+    btn_edit.click(
+        fn=apply_class_feature,
+        inputs=[base_state, base_ratio, normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark, steel, fairy],
         outputs=output_img,
         concurrency_id='gpu'
+    ).success(
+        fn=add_image_to_gallery,
+        inputs=[output_gallery, output_img],
+        outputs=output_gallery
     )
 
+demo.title = 'Pokémon Generator'
 demo.queue(default_concurrency_limit=1)
 demo.launch(show_api=False)
--- generate.py
+++ generate.py
@@ -1,15 +1,16 @@
-import sys
-sys.path.append('./stylegan-xl')
-
-import os, glob, copy
-import dill, imageio
+import torch
 import numpy as np
 from PIL import Image
-import torch
+import random
+
+import sys
+sys.path.append('./stylegan-xl')
+from torch_utils import gen_utils
 import dnnlib, legacy
 
 NETWORK = "https://s3.eu-central-1.amazonaws.com/avg-projects/stylegan_xl/models/pokemon1024.pkl"
-SEED = 42
+SEED = random.randint(0, 1000000)
+TRUNCATION_PSI = 1.0
 
 np.random.seed(SEED)
 torch.manual_seed(SEED)
@@ -22,8 +23,14 @@ with dnnlib.util.open_url(NETWORK) as f:
     G = G.eval().requires_grad_(False).to(device)
 
 # -------------------------------------------------------------------------
+# 특정 class의 이미지 생성
 
-vector_path = 'class_vector/Fairy.npy'
+class_en = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
+
+class_name = class_en[1]
+print(f'class_name : {class_name}')
+
+vector_path = f'class_features/{class_name}.npy'
 projected_w = np.load(vector_path)
 projected_w = torch.from_numpy(projected_w).to(device)
 projected_w.shape # (1, 1, 512)
@@ -31,74 +38,77 @@ projected_w.shape # (1, 1, 512)
 # projected_w = projected_w[0]
 # projected_w.repeat(1, G.num_ws, 1).shape
 
-synth_image = G.synthesis(projected_w.repeat(1, G.num_ws, 1))
-synth_image = (synth_image + 1) * (255/2)
-synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
-Image.fromarray(synth_image, 'RGB')
+projected_w = projected_w.repeat(1, G.num_ws, 1)
+output = gen_utils.w_to_img(G, projected_w, to_np=True)[0]
+Image.fromarray(output, 'RGB')
 
 # -------------------------------------------------------------------------
+# 각 class에 가중치를 적용하여 생성
 
-class_en = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel", "Fairy"]
-
-normal = 0
-fire = 0.1
-water = 0.12
-grass = 1
-electric = 0.1
-ice = 0.1
-fighting = 0.1
-poison = 0
-ground = 0.1
-flying = 0
-psychic = 0
-bug = 0.1
-rock = 0
-ghost = 0
-dragon = 0.1
-dark = 1
-steel = 0.1
-fairy = 0.1
+normal = round(random.random(), 2)
+fire = round(random.random(), 2)
+water = round(random.random(), 2)
+grass = round(random.random(), 2)
+electric = round(random.random(), 2)
+ice = round(random.random(), 2)
+fighting = round(random.random(), 2)
+poison = round(random.random(), 2)
+ground = round(random.random(), 2)
+flying = round(random.random(), 2)
+psychic = round(random.random(), 2)
+bug = round(random.random(), 2)
+rock = round(random.random(), 2)
+ghost = round(random.random(), 2)
+dragon = round(random.random(), 2)
+dark = round(random.random(), 2)
+steel = round(random.random(), 2)
+fairy = round(random.random(), 2)
 
 values = [normal, fire, water, grass, electric, ice, fighting, poison, ground, flying, psychic, bug, rock, ghost, dragon, dark,steel, fairy]
 values = np.array(values)
 values_normalized = values / values.sum()
 
-# -------------------------------------------------------------------------
-# class vector를 그대로 사용하면 생각보다 생성 퀄리티가 별로라 베이스가 되는 벡터가 있는게 낫지 않나..?
-
-base_vector = 
-w_avg_samples = 10000
-print(f'Computing W midpoint and stddev using {w_avg_samples} samples...')
-z_samples = torch.from_numpy(np.random.RandomState(123).randn(w_avg_samples, G.z_dim)).to(device)
-w_samples = G.mapping(z_samples, c_samples)  # [N, L, C]
-
-w = gen_utils.get_w_from_seed(G, batch_sz, device, truncation_psi, seed=seed,
-                                centroids_path=centroids_path, class_idx=class_idx)
-
-w = gen_utils.get_w_from_seed(G, batch_sz, device, truncation_psi, seed=seed,
-                                centroids_path=centroids_path, class_idx=class_idx)
-img = gen_utils.w_to_img(G, w, to_np=True)
-
-# -------------------------------------------------------------------------
-
-
-
-
 target_vector = None
 for i, value_normalized in enumerate(values_normalized) :
     class_name = class_en[i]
-    class_vector = np.load(f'class_vector/{class_name}.npy')
+    class_feature = np.load(f'class_features/{class_name}.npy')
 
     if target_vector is None :
-        target_vector = class_vector * value_normalized
+        target_vector = class_feature * value_normalized
     else :
-        target_vector += class_vector * value_normalized
+        target_vector += class_feature * value_normalized
 
 projected_w = target_vector.copy()
 projected_w = torch.from_numpy(projected_w).to(device)
 projected_w.shape # (1, 1, 512)
 
-synth_image = G.synthesis(projected_w.repeat(1, G.num_ws, 1))
-synth_image = (synth_image + 1) * (255/2)
-synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
-Image.fromarray(synth_image, 'RGB')
+projected_w = projected_w.repeat(1, G.num_ws, 1)
+output = gen_utils.w_to_img(G, projected_w, to_np=True)[0]
+Image.fromarray(output, 'RGB')
+
+## class vector를 그대로 사용하면 예상과 다르게 다양성이 없어지는 문제 발생
+## 따라서, 베이스 벡터를 사용하여 베이스 이미지를 생성한 후 작업하는 방향으로 변경
+
+# -------------------------------------------------------------------------
+# 무작위로 베이스 벡터를 통한 이미지 생성
+SEED = random.randint(0, 1000000)
+
+base_w = gen_utils.get_w_from_seed(G, batch_sz=1, device=device, seed=SEED)
+base_w.shape
+
+output = gen_utils.w_to_img(G, base_w, to_np=True)[0]
+output.shape
+
+output_img = Image.fromarray(output, 'RGB')
+output_img
+
+# -------------------------------------------------------------------------
+# 위 베이스 벡터를 기반으로 클래스 벡터를 적용
+
+base_ratio = 0.5
+edit_w = (base_w * base_ratio) + projected_w * (1 - base_ratio)
+
+output_edit = gen_utils.w_to_img(G, edit_w, to_np=True)[0]
+output_edit.shape
+
+Image.fromarray(output_edit, 'RGB')
